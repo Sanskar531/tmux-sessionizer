44c6599c2cbac3ad034ad0ba94508fbc4643fb0b
Sanskar531 <sanskargauchanb4@gmail.com>
2023-03-05 14:02:52 +1100

    Add logic to extract branches

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::process::Command;
+
+#[derive(Clone)]
+struct Branch<'a> {
+    name: &'a str,
+}
+
+impl<'a> Branch<'a> {
+    fn clean(&mut self) {
+        self.name = &self.name[2..];
+    }
+}
+
+fn check_installed_commands() {
+    Command::new("git").output().expect("Please install git");
+    Command::new("tmux").output().expect("Please install tmux");
+}
+
 fn main() {
-    println!("Hello, world!");
+    check_installed_commands();
+    let process_output =
+        String::from_utf8(Command::new("git").arg("branch").output().unwrap().stdout).unwrap();
+    let branches: Vec<Branch> = process_output
+        .split("\n")
+        .filter(|branch| branch.len() > 2)
+        .map(|branch| -> Branch {
+            let mut branch = Branch { name: branch };
+            branch.clean();
+            branch
+        })
+        .collect();
+    branches.iter().for_each(|x| println!("{}", x.name));
 }
