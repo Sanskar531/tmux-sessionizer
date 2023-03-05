b922b2822d3ca7f8e0168fdc292c105d706c3faa
Sanskar531 <sanskargauchanb4@gmail.com>
2023-03-05 16:00:36 +1100

    Pipe output from git directly to fzf

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,33 @@
-use std::process::Command;
-
-#[derive(Clone)]
-struct Branch<'a> {
-    name: &'a str,
-}
-
-impl<'a> Branch<'a> {
-    fn clean(&mut self) {
-        self.name = &self.name[2..];
-    }
-}
-
-fn check_installed_commands() {
-    Command::new("git").output().expect("Please install git");
-    Command::new("tmux").output().expect("Please install tmux");
-}
+use std::{
+    io::Write,
+    process::{Command, Stdio},
+};
 
 fn main() {
-    check_installed_commands();
-    let process_output =
-        String::from_utf8(Command::new("git").arg("branch").output().unwrap().stdout).unwrap();
-    let branches: Vec<Branch> = process_output
-        .split("\n")
-        .filter(|branch| branch.len() > 2)
-        .map(|branch| -> Branch {
-            let mut branch = Branch { name: branch };
-            branch.clean();
-            branch
-        })
-        .collect();
-    branches.iter().for_each(|x| println!("{}", x.name));
+    let git_command = Command::new("git")
+        .arg("branch")
+        .output()
+        .expect("Failed to launch git command");
+    let git_output = String::from_utf8(git_command.stdout).unwrap();
+    let mut fzf_command = match Command::new("fzf")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+    {
+        Err(e) => panic!("{}", e),
+        Ok(command) => command,
+    };
+
+    match fzf_command
+        .stdin
+        .take()
+        .unwrap()
+        .write_all(git_output.as_bytes())
+    {
+        Err(e) => panic!("Error: {}", e),
+        _ => (),
+    }
+
+    let branch_name = String::from_utf8(fzf_command.wait_with_output().unwrap().stdout).unwrap();
+    let _clean_branch_name = &branch_name[2..];
 }
