8c838461d6ee8109d5c77e1a4030440317b648cf
Sanskar531 <sanskargauchanb4@gmail.com>
2023-03-21 20:49:54 +1100

    Symlink yarn dependencies if they exist and remove unused files

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,10 @@ fn main() {
     let mut worktree_service = WorktreeService { git_service };
     worktree_service.create_worktree();
     let branch = worktree_service.git_service.get_branch().unwrap();
-    TmuxService::spawn_session(branch);
-    // TODO: create symlinks for dependencies
-
     let mut directories = read_dir("./").unwrap();
     let does_node_modules_exist = directories.any(|dir| dir.unwrap().file_name() == "node_modules");
     if does_node_modules_exist {
         SymLink::link_folder("node_modules", branch);
     }
+    TmuxService::spawn_session(branch);
 }
--- src/symlink/mod.rs
+++ src/symlink/mod.rs
@@ -4,16 +4,20 @@ pub struct SymLink;
 
 impl SymLink {
     pub fn link_folder(folder_name: &str, branch: &str) {
+        let path_to_folder = format!(
+            "{}/{}/",
+            current_dir().unwrap().to_str().unwrap(),
+            folder_name
+        );
+        let path_to_create_link = format!(
+            "/home/sanskar/git-worktrees/{}/{}",
+            branch,
+            folder_name
+        );
         SymLink::get_sym_link_command()
             .arg("-s")
-            .arg("-t")
-            .arg(format!("/home/sanskar/git-worktrees/{}/", branch))
-            .arg(folder_name)
-            .arg(format!(
-                "{}/{}/",
-                current_dir().unwrap().to_str().unwrap(),
-                folder_name
-            ))
+            .arg(path_to_folder)
+            .arg(path_to_create_link)
             .output()
             .unwrap();
     }
